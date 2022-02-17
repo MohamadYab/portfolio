5efc30e152622d95b21a56697c694ad8798d8fa2
Mohamad Yabroudi <mohamad.syr7@gmail.com>
2022-02-17 18:12:49 +0000

    Insert ViewportProvider into index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ViewportProvider } from './use-Viewport';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ViewportProvider>
+      <App />
+    </ViewportProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
