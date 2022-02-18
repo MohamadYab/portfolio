5fba2c2c19be7b33381f9ffa1e6c864a284efe3f
Mohamad Yabroudi <mohamad.syr7@gmail.com>
2022-02-18 23:23:45 +0000

    Create/Add Logo

---
+++ src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Typing from '../icons/Typing.js';
+
+const Logo = () => {
+    return (
+        <div className='logo-container'>
+            <p className='logo'>MOHAMAD YABROUDI.</p>
+            <Typing/>
+        </div>
+    );
+}
+
+export default Logo;
