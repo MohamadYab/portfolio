6f39a19e81397d56295300faa689ba77511c28a4
Mohamad Yabroudi <mohamad.syr7@gmail.com>
2022-02-23 22:28:32 +0000

    Update Logo.js

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Typing from '../icons/Typing.js';
 
 const Logo = () => {
     return (
         <div className='logo-container'>
             <p className='logo'>MOHAMAD YABROUDI.</p>
-            <Typing/>
         </div>
     );
 }
-
 export default Logo;
