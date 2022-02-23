ccf6144ba8b97da6f2a16da821d5cd3347b63d74
Mohamad Yabroudi <mohamad.syr7@gmail.com>
2022-02-23 22:29:11 +0000

    Add Rose image and Update contact section

--- src/components/Desktop.js
+++ src/components/Desktop.js
@@ -2,13 +2,24 @@ import React from 'react';
 import '../App.css';
 import { useViewport } from '../use-Viewport';
 import Logo from '../components/Logo.js'
-import Hamburger from '../icons/Hamburger';
+import ContactForm from './ContactForm';
+import Socials from './Socials';
 
 function Desktop() {
   const { isDesktop } = useViewport();
   return (
     <div className="App">
-      Desktop Here
+      <div className='desktop-contact'>
+        <div className='gradient '>
+          <div className='rose-image'>
+            <div className='container desktop-contact-info'>
+              <Logo />
+              <ContactForm />
+              <Socials />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
