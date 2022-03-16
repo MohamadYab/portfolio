1ee66142cb8aeb8978ebd3879066344030dfa866
Mohamad Yabroudi <mohamad.syr7@gmail.com>
2022-03-16 23:45:33 +0000

    Add Tabs to Desktop Component

--- src/components/Desktop.js
+++ src/components/Desktop.js
@@ -4,20 +4,35 @@ import { useViewport } from '../use-Viewport';
 import Logo from '../components/Logo.js'
 import ContactForm from './ContactForm';
 import Socials from './Socials';
+import Tabs from './Tabs';
 
 function Desktop() {
   const { isDesktop } = useViewport();
+  const test = () => {
+   
+  }
+
   return (
-    <div className="App">
+    <div className="App desktop">
+      {/** Left Contact Section */}
       <div className='desktop-contact'>
         <div className='gradient '>
           <div className='rose-image'>
-            <div className='container desktop-contact-info'>
-              <Logo />
-              <ContactForm />
-              <Socials />
+            <div className="container">
+              <div className='desktop-contact-info'>
+                <Logo />
+                <ContactForm />
+                <Socials />
+              </div>
             </div>
           </div>
+        </div>
+      </div>
+
+      {/** Content Section */}
+      <div className='desktop'>
+        <div className="container red" onClick={test}>
+          <Tabs />
         </div>
       </div>
     </div>
