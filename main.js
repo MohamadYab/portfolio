e315b360f4577f7c249c4962763d79e3668de619
Mohamad Yabroudi <mohamad.syr7@gmail.com>
2022-05-12 15:04:35 +0100

    Implement mobile nav functionality

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 "use strict"
+
+/** Selectors */
+const menuBtn = document.querySelector('.menu-btn'),
+mobileNav = document.querySelector('.navigation'),
+aboutPage = document.querySelector('#about'),
+workPage = document.querySelector('#about'),
+contactPage = document.querySelector('#about');
+
+/** Functions */
+
+/** Display navigation section by toggling classes to elements Classlists */
+const toggleMobileNav = () => {
+    menuBtn.classList.toggle('open');
+    mobileNav.classList.toggle('open-nav');
+}
+
+
+/** Event Listeners */
+menuBtn.addEventListener('click', toggleMobileNav);
