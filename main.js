6cc81182e0dbc052d2349f78e0b286d1b9cd6c8a
Mohamad Yabroudi <mohamad.syr7@gmail.com>
2022-05-21 18:33:59 +0100

    Update Contact Form

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ aboutPage = document.querySelector('#about'),
 workPage = document.querySelector('#work'),
 contactPage = document.querySelector('#contact'),
 pages = document.querySelectorAll('.page'),
-navItems = document.querySelectorAll('.nav-item');
+navItems = document.querySelectorAll('.nav-item'),
+form = document.querySelector('#form');
 
 /** Functions */
 const activeNavHandler = (currectPageId) => {
@@ -35,10 +36,36 @@ const jumptToSection = (item) => {
             break;
     }
 }
+
 /** Display navigation section by toggling classes to elements Classlists */
 const toggleMobileNav = () => {
     menuBtn.classList.toggle('open');
     mobileNav.classList.toggle('open-nav');
+}
+
+/** Sending Form */
+const submitForm = (e) => {
+    e.preventDefault();
+    console.log('test');
+
+    const formData = new FormData(form);
+    formData.append('service_id', 'gmail');
+    formData.append('template_id', 'gmail');
+    formData.append('user_id', 'VS0JMyFRa7-cuS9HL');
+
+    const xhttp = new XMLHttpRequest(),
+    url = 'https://api.emailjs.com/api/v1.0/email/send-form';
+
+    xhttp.open('POST', url, true);
+    xhttp.send(formData);
+
+    xhttp.onreadystatechange = () => {
+        if(xhttp.status != 200) {
+            console.log('Something went wrong');
+            return;
+        };
+        console.log('Working');
+    }
 }
 
 /** Intersection observer */
@@ -66,3 +93,5 @@ navItems.forEach(item => {
         toggleMobileNav();
     });
 })
+
+form.addEventListener('submit', submitForm);
