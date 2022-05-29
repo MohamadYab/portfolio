51d8751fc279ad29e2fd7501c39517ff602bc27e
Mohamad Yabroudi <mohamad.syr7@gmail.com>
2022-05-30 00:30:04 +0100

    update about page with the new design

--- main.js
+++ main.js
@@ -1,31 +1,13 @@
 "use strict"
 
 /** Selectors */
-const menuBtn = document.querySelector('.menu-btn'),
-mobileNav = document.querySelector('.navigation'),
-aboutPage = document.querySelector('#about'),
-workPage = document.querySelector('#work'),
+const workPage = document.querySelector('#work'),
 contactPage = document.querySelector('#contact'),
-pages = document.querySelectorAll('.page'),
 navItems = document.querySelectorAll('.nav-item'),
 form = document.querySelector('#form');
 
-/** Functions */
-const activeNavHandler = (currectPageId) => {
-    navItems.forEach(item => {
-        if(item.dataset.page === currectPageId) {
-            item.classList.add('active-nav-item');
-            return;
-        }
-        item.classList.remove('active-nav-item');
-    })
-}
-
 const jumptToSection = (item) => {
     switch (item.dataset.page) {
-        case 'about':
-            aboutPage.scrollIntoView();
-            break;
         case 'work':
             workPage.scrollIntoView();
             break;
@@ -35,12 +17,6 @@ const jumptToSection = (item) => {
         default:
             break;
     }
-}
-
-/** Display navigation section by toggling classes to elements Classlists */
-const toggleMobileNav = () => {
-    menuBtn.classList.toggle('open');
-    mobileNav.classList.toggle('open-nav');
 }
 
 /** Sending Form */
@@ -64,33 +40,14 @@ const submitForm = (e) => {
             console.log('Something went wrong');
             return;
         };
-        console.log('Working');
     }
 }
 
-/** Intersection observer */
-const callback = (section, sectionWatcher) => {
-    section.forEach(section => {
-        if(!section.isIntersecting) return;
-        activeNavHandler(section.target.id)
-    })
-}
-
-const sectionWatcher = new IntersectionObserver (callback, {
-    threshold: .6
-});
-
-pages.forEach(page => {
-    sectionWatcher.observe(page);
-});
-
 /** Event Listeners */
-menuBtn.addEventListener('click', toggleMobileNav);
 navItems.forEach(item => {
     item.addEventListener('click', (e) => {
         e.stopPropagation();
         jumptToSection(item)
-        toggleMobileNav();
     });
 })
 
